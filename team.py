124b267009097f31459700c76a528c039b9fdd18
Lindsay Ward <lindsay.ward@jcu.edu.au>
2018-04-13 10:37:43 +1000

    Add and test get_leader and get_total_score in Team class

--- team.py
+++ team.py
@@ -10,3 +10,15 @@ class Team:
 
     def add(self, user):
         self.users.append(user)
+
+    def get_leader(self):
+        highest_score = -1
+        leader = None
+        for user in self.users:
+            if user.score > highest_score:
+                highest_score = user.score
+                leader = user
+        return leader
+
+    def get_total_score(self):
+        return sum([user.score for user in self.users])
--- test_team.py
+++ test_team.py
@@ -5,13 +5,22 @@ from user import User
 def run_tests():
     team1 = Team()
     user1 = User("Jenny")
+    user2 = User("Lukas")
+    number_to_give = 3
+    user1.give_tacos(user2, number_to_give)
     print(team1)
     assert team1.users == []
     assert not team1.users
 
     team1.add(user1)
+    team1.add(user2)
     assert team1.users
     print(team1.users)
+
+    print(team1.get_leader())
+
+    print("Total score expected: {}; got {}".format(number_to_give, team1.get_total_score()))
+    assert team1.get_total_score() == number_to_give
 
 
 run_tests()
