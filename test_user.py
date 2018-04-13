c9893c38f5aa9ad882dd46786d04f8114557cc44
Lindsay Ward <lindsay.ward@jcu.edu.au>
2018-04-13 10:07:36 +1000

    Add and test give_tacos method

--- test_user.py
+++ test_user.py
@@ -3,11 +3,16 @@ from user import User
 
 
 def run_tests():
-    user = User()
-    print("number should be 5; is {}".format(user.number_of_tacos))
-    assert user.number_of_tacos == 5, "problem"
+    user1 = User()
+    print("number should be 5; is {}".format(user1.number_of_tacos))
+    assert user1.number_of_tacos == 5, "problem"
 
     user2 = User("Ben")
+    print(user2)
+
+    #     test give_tacos
+    user1.give_tacos(user2, -7)
+    print(user1)
     print(user2)
 
 
--- user.py
+++ user.py
@@ -11,3 +11,11 @@ class User:
 
     def __str__(self):
         return "{}, {} points, {} tacos left".format(self.name, self.score, self.number_of_tacos)
+
+    def give_tacos(self, other_user, number_to_give):
+        if self.number_of_tacos < number_to_give:
+            number_to_give = self.number_of_tacos
+        elif number_to_give < 0:
+            number_to_give = 0
+        self.number_of_tacos -= number_to_give
+        other_user.score += number_to_give
